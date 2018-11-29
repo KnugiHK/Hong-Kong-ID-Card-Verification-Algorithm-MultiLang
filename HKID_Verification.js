19a5b79bfa531d446f932ade13acdac12a036745
Knugi <24708955+knugi0123@users.noreply.github.com>
2018-11-29 21:58:38 +0800

    Convert Code
    Convert latest algorithm of Python version to JavaScript

--- HKID_Verification.js
+++ HKID_Verification.js
@@ -1,59 +1,45 @@
-//Hong Kong ID Card verification
+//////Hong Kong ID Card verification//////
 function HKIDverification(){
-	//Library
-	convertion = {
-	  "A": 1,
-	  "B": 2,
-	  "C": 3,
-	  "D": 4,
-	  "E": 5,
-	  "F": 6,
-	  "G": 7,
-	  "H": 8,
-	  "I": 9,
-	  "J": 10,
-	  "K": 11,
-	  "L": 12,
-	  "M": 13,
-	  "N": 14,
-	  "O": 15,
-	  "P": 16,
-	  "Q": 17,
-	  "R": 18,
-	  "S": 19,
-	  "T": 20,
-	  "U": 21,
-	  "V": 22,
-	  "W": 23,
-	  "X": 24,
-	  "Y": 25,
-	  "Z": 26
-	};
-
-	// Pre-Operation
-	var id = document.getElementById("hkid").value;
-	var productNsum, remainder, check;
-	if(id.length == 8){
-		letter = convertion[id[0].toUpperCase()];
-		
-		//Operation
-		var productNsum = letter * 8 + parseInt(id[1]) * 7 + parseInt(id[2]) * 6 + parseInt(id[3]) * 5 + parseInt(id[4]) * 4 + parseInt(id[5]) * 3 + parseInt(id[6]) * 2;
-		var remainder = productNsum % 11;
-
-		if(remainder == 10){
-			check = "A"
-		}else if(remainder == 0){
-			check = "0"
-		}else{
-			check = 11 - remainder
-		}
+	//////Pre-Operation//////
+	
+	//Let user input a ID Card number
+	id = document.getElementById("HKID").value.toString();
+	//Check if the value inputted by user is a vaild
+	if(id[0].charCodeAt() >= 65 && id[0].charCodeAt() <= 90){
+		if(id.length == 8){
+			//Convert lower case letter to upper case
+			letter = id[0].toUpperCase();
+			//Convert letter to number for later on calculation by ASCII - 64
+			convert = letter.charCodeAt() - 64;
 			
-		if(parseInt(id[7]) == check){
-			alert("You provided a vaild Hong Kong ID Card number.")
+			//////Operation//////
+			
+			//Calculate product and sum of user inputted ID Card number
+			productNsum = convert * 8 + parseInt(id[1]) * 7 + parseInt(id[2]) * 6 + parseInt(id[3]) * 5 + parseInt(id[4]) * 4 + parseInt(id[5]) * 3 + parseInt(id[6]) * 2;
+			//Find remainder of calculated product and sum
+			remainder = productNsum % 11;
+			//Default check digit is zero
+			check = 0;
+			//If remainder is not zero, run the following code
+			if (remainder != 0){
+				//Check digit is 11 - remainder
+				check = 11 - remainder;
+				//If check digit is 10, show "A" as check digit
+				if(check == 10){
+					check = "A";
+				}
+			}
+			//Send back the result to user
+			if(id[7] == check.toString()){
+				alert("You provided a vaild Hong Kong ID Card number.");
+			}else{
+				alert("You provided a incorrect Hong Kong ID Card number.");
+			}
+				
 		}else{
-			alert("You provided a incorrect Hong Kong ID Card number.")
-		}		
+			alert("Please provide a complete ID Card number."); //In case, user did not provide a complete ID Card number
+		} 
 	}else{
-		alert("Please provide a complete ID Card number.")
-	}	
+		alert("Please provide a complete ID Card number."); //In case, user did not provide a complete ID Card number
+	} 
 }
