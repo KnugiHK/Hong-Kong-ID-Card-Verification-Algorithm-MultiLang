abbb274ec63c1416d5cad6dfad04172a1c59a138
Knugi <24708955+knugi0123@users.noreply.github.com>
2018-11-30 20:00:29 +0800

    Big Fix, Optimizing Code and New Feature
    Big Fix:
    1. User input a lower case ID Card number but program continue to run.
    Optimizing Code:
    1.Program just need to run ord() once only.
    New Feature:
    1.User can now type in lower case
    2.Support ID Card number with two letter

--- HKID_Verification.py
+++ HKID_Verification.py
@@ -8,22 +8,69 @@ def HKIDverification(id):
 	
 	"""When using console assign the following:
 	id = "str(getpass.getpass('Please provide your Hong Kong ID Card number including letter and digit in bracket such as "L5555550" (For security reason, value you typed will not be displayed):'))" """
-	#Check if the value inputted by user is a vaild
-	if ord(id[0]) >= 65 & ord(id[0]) <= 90:
-		if len(id) == 8:
+	#Check if the value inputted by user is a vaild and if the value consist 8 character, do the following
+	if len(id) == 8:
+		#Convert character to ASCII code
+		letterASCII = ord(id[0])
+		#Check if the value inputted by user is a vaild
+		if letterASCII >= 65 and letterASCII <= 90 or letterASCII >= 97 and letterASCII <= 122:
 			#Convert lower case letter to upper case
-			letter = id[0].upper()
+			if letterASCII >= 97 and letterASCII <= 122:
+				#Set character to upper case with ASCII code
+				letterASCII = letterASCII - 32
 			#Convert letter to number for later on calculation by ASCII - 64
-			convert = ord(letter) - 64
+			converted = letterASCII - 64
 			
 			###Operation###
 			
 			#Calculate product and sum of user inputted ID Card number
-			productNsum = convert * 8 + int(id[1]) * 7 + int(id[2]) * 6 + int(id[3]) * 5 + int(id[4]) * 4 + int(id[5]) * 3 + int(id[6]) * 2
+			productNsum = converted * 8 + int(id[1]) * 7 + int(id[2]) * 6 + int(id[3]) * 5 + int(id[4]) * 4 + int(id[5]) * 3 + int(id[6]) * 2
 			#Find remainder of calculated product and sum
 			remainder = productNsum % 11
 			#Default check digit is zero
 			check = 0
+			check_2 = 0
+			#If remainder is not zero, run the following code
+			if remainder != 0:
+				#Check digit is 11 - remainder
+				check = 11 - remainder
+				check_2 = check
+				#If check digit is 10, show "A" as check digit
+				if check == 10:
+					check = "A"
+					check_2 = "a"
+			#Send back the result to user
+			if id[7] == str(check) or id[7] == check_2:
+				print("You provided a vaild Hong Kong ID Card number.")
+			else:
+				print("You provided a incorrect Hong Kong ID Card number.")
+		else:
+			print("Please provide a complete ID Card number.")#In case, user did not provide a complete ID Card number
+	#Check if the value inputted by user is a vaild and if the value consist 9 character, do the following
+	elif len(id) == 9:
+		#Convert character to ASCII code
+		letterASCII_1 = ord(id[0])
+		letterASCII_2 = ord(id[1])
+		#Check if the value inputted by user is a vaild
+		if (letterASCII_1 >= 65 and letterASCII_1 <= 90 or letterASCII_1 >= 97 and letterASCII_1 <= 122) and (letterASCII_2 >= 65 and letterASCII_2 <= 90 or letterASCII_2 >= 97 and letterASCII_2 <= 122):
+			#Convert lower case letter to upper case
+			if (letterASCII_1 >= 97 and letterASCII_1 <= 122) or (letterASCII_2 >= 97 and letterASCII_2 <= 122):
+				#Set character to upper case with ASCII code
+				letterASCII_1 = letterASCII_1 - 32
+				letterASCII_2 = letterASCII_2 - 32
+			#Convert letter to number for later on calculation by ASCII - 64
+			converted_1 = letterASCII_1 - 64
+			converted_2 = letterASCII_2 - 64
+			
+			###Operation###
+			
+			#Calculate product and sum of user inputted ID Card number
+			productNsum = converted_1 * 9 + converted_2 * 8 + int(id[2]) * 7 + int(id[3]) * 6 + int(id[4]) * 5 + int(id[5]) * 4 + int(id[6]) * 3 + int(id[7]) * 2
+			#Find remainder of calculated product and sum
+			remainder = productNsum % 10
+			#Default check digit is zero
+			check = 0
+			check_2 = 0
 			#If remainder is not zero, run the following code
 			if remainder != 0:
 				#Check digit is 11 - remainder
@@ -31,8 +78,9 @@ def HKIDverification(id):
 				#If check digit is 10, show "A" as check digit
 				if check == 10:
 					check = "A"
+					check_2 = "a"
 			#Send back the result to user
-			if id[7] == str(check):
+			if id[8] == str(check) or id[8] == str(check_2):
 				print("You provided a vaild Hong Kong ID Card number.")
 			else:
 				print("You provided a incorrect Hong Kong ID Card number.")
