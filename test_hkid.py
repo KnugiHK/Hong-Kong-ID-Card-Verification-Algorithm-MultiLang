efa1ec2883f506e8e36f964570ed4457ea41b030
Knugi <24708955+KnugiHK@users.noreply.github.com>
2022-04-04 10:23:02 +0000

    Update test_hkid.py

--- test_hkid.py
+++ test_hkid.py
@@ -1,25 +1,23 @@
 from HKID_Verification import verify, calculate
+import pytest
 
 def test_valid():
-    assert verify("C1234569") == True
-    assert verify("G123456A") == True
+    assert verify("C1234569")
+    assert verify("G123456A")
     
 def test_invalid():
-    assert verify("AY987654A") == False
-    assert verify("C1234567") == False
+    assert not verify("AY987654A")
+    assert not verify("C1234567")
 
 def test_cal_correct():
     hkid1 = "C123456"
     hkid2 = "S455585"
-    assert verify(hkid1 + calculate(hkid1)) == True
-    assert verify(hkid2 + calculate(hkid2)) == True
+    assert verify(hkid1 + calculate(hkid1))
+    assert verify(hkid2 + calculate(hkid2))
 
 def test_incorrect():
-    try:
+    with pytest.raises(ValueError):
         verify("A123456789")
         verify("asdasd")
         verify("")
-    except ValueError:
-        _pass = False
-    assert _pass == False
     
