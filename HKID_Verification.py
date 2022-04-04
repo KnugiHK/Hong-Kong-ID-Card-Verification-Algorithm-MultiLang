fb0b767ade0b990f083f46b74a98f722cad7c49e
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2022-04-04 23:19:37 +0800

    PEP8

--- HKID_Verification.py
+++ HKID_Verification.py
@@ -1,9 +1,6 @@
-"""LastUpdate:1593676941|Powered by Simple Clock. Check it out on https://github.com/knugi0123/SimpleClock
-Hong Kong ID Card verification"""
-
 """MIT License
 
-Copyright (c) 2020 Knugi
+Copyright (c) 2022 Knugi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,8 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
-#Library
-BasicInfo = {
+basic_info = {
     "A": "issued between 1949 and 1962.",
     "B": "issued in the urban office between 1955 and 1960.",
     "C": "issued at the New Territories Office between 1960 and 1983.",
@@ -54,11 +50,14 @@ BasicInfo = {
     "XG": "issued for persons who registered a new ID without Chinese name.",
     "Y": "issued for persons registered in Hong Kong from January 1, 1989 to March 2005.",
     "Z": "issued for persons registered in Hong Kong from January 1, 1980 to December 31, 1988.",
-    "default": "not in our database."    
+    "default": "not in our database."
 }
 
+BasicInfo = basic_info
+
+
 def determine_divisor(hkid):
-    #Check if the value entered by user is vaild
+    # Check if the value entered by user is vaild
     length = len(hkid)
     if length == 8 or length == 7:
         if hkid[1].isdigit():
@@ -76,23 +75,24 @@ def determine_divisor(hkid):
             raise ValueError("Number of characters of HKID should be 7 to 9.")
     else:
         raise ValueError("Number of characters of HKID should be 7 to 9.")
-    
+
     return divisor, hkid, length
+
 
 def letter_to_number(hkid):
     divisor, hkid, length = determine_divisor(hkid)
 
-    #Convert character to ASCII code
+    # Convert character to ASCII code
     letterASCII_1 = ord(hkid[0])
     letterASCII_2 = ord(hkid[1])
     if (letterASCII_1 >= 65 and letterASCII_1 <= 90 or letterASCII_1 >= 97 and letterASCII_1 <= 122 or letterASCII_1 == 48) and (letterASCII_2 >= 65 and letterASCII_2 <= 90 or letterASCII_2 >= 97 and letterASCII_2 <= 122):
-        #Convert lower case letter to upper case
+        # Convert lower case letter to upper case
         if (letterASCII_1 >= 97 and letterASCII_1 <= 122) or (letterASCII_2 >= 97 and letterASCII_2 <= 122):
-            #Set character to upper case with ASCII code
+            # Set character to upper case with ASCII code
             letterASCII_1 = letterASCII_1 - 32
             letterASCII_2 = letterASCII_2 - 32
-            
-        #Convert letter to number for later on calculation by ASCII - 64
+
+        # Convert letter to number for later on calculation by ASCII - 64
         converted_1 = 0
         if length == 9:
             converted_1 = letterASCII_1 - 64
@@ -101,72 +101,90 @@ def letter_to_number(hkid):
     else:
         raise ValueError("First character of HKID should be contain A-Z only.")
 
+
 def cal_check(remainder):
-    #Default check digit is zero
+    # Default check digit is zero
     check = 0
-    check_2 = 0 #In case the user enter a lower case HKID Card number
-    #If remainder is not zero, run the following code
+    check_2 = 0  # In case the user enter a lower case HKID Card number
+    # If remainder is not zero, run the following code
     if remainder != 0:
-        #Check digit is 11 - remainder
+        # Check digit is 11 - remainder
         check = 11 - remainder
         check_2 = check
-        #If check digit is 10, show "A" as check digit
+        # If check digit is 10, show "A" as check digit
         if check == 10:
             check = "A"
             check_2 = "a"
     return [check, check_2]
 
+
 def cal_remainder(converted, divisor, hkid):
-    #Calculate product and sum of user inputted ID Card number
+    # Calculate product and sum of user inputted ID Card number
     productNsum = converted[0] * 9 + converted[1] * 8 + int(hkid[2]) * 7 + int(hkid[3]) * 6 + int(hkid[4]) * 5 + int(hkid[5]) * 4 + int(hkid[6]) * 3 + int(hkid[7]) * 2
 
-    #Find remainder of calculated product and sum
+    # Find remainder of calculated product and sum
     remainder = productNsum % divisor
     return remainder
 
+
 def calculate(hkid):
     converted, divisor, hkid = letter_to_number(hkid)
-    
+
     remainder = cal_remainder(converted, divisor, hkid)
-    
+
     check = cal_check(remainder)
-    
+
     return str(check[0])
 
+
 def verify(hkid):
-    #Check if the value inputted by user is a vaild and if the value consist 8 character, do the following
+    # Check if the value inputted by user is a vaild and
+    # if the value consist 8 character, do the following
     converted, divisor, hkid = letter_to_number(hkid)
-    
+
     remainder = cal_remainder(converted, divisor, hkid)
-    
+
     check = cal_check(remainder)
     if hkid[-1] == str(check[0]) or hkid[-1] == str(check[1]):
         return True
     else:
         return False
 
-    #For CSharp and JavaScript only 
-    #type = BasicInfo["default"];
-    #if(hkid[0] + hkid[1]) in BasicInfo:
-    #type = BasicInfo[hkid[0] + hkid[1]];
-    
+    # For CSharp and JavaScript only
+    # type = basic_info["default"];
+    # if(hkid[0] + hkid[1]) in basic_info:
+    # type = basic_info[hkid[0] + hkid[1]];
+
 
 if __name__ == "__main__":
     from getpass import getpass
 
-    hkid = str(getpass('Please provide your Hong Kong ID Card number including letter and digit in bracket such as "L5555550" (For security reason, value you typed will not be displayed):'))
-    
+    hkid = str(
+        getpass(
+            'Please provide your Hong Kong ID Card number including '
+            'letter and digit in bracket such as "L5555550" (For '
+            "security reason, value you typed will not be displayed):"
+        )
+    )
+
     try:
         valid = verify(hkid)
     except Exception as e:
         print(str(e))
-        print("Please provide a ID Card number with correct format.")#In case, user did not provide a complete ID Card number
+        # In case, user did not provide a complete ID Card number
+        print("Please provide a ID Card number with correct format.")
     else:
         if valid:
             print("You provided a vaild Hong Kong ID Card number.")
             if len(hkid) == 8:
-                print("Category of this ID Card is '" + hkid[0] + "', which is " + BasicInfo.setdefault(hkid[0], BasicInfo["default"]))
+                print(
+                    f"Category of this ID Card is '{hkid[0]}', which is "
+                    f"{basic_info.get(hkid[0], basic_info['default'])}"
+                )
             elif len(hkid) == 9:
-                print("Category of this ID Card is '" + hkid[0] + hkid[1] + "', which is " + BasicInfo.setdefault(hkid[0] + hkid[1], BasicInfo["default"]))
+                print(
+                    f"Category of this ID Card is '{hkid[0]}{hkid[1]}', which is "
+                    f"{basic_info.get(hkid[0] + hkid[1], basic_info['default'])}"
+                )
         else:
             print("You provided an incorrect Hong Kong ID Card number.")
